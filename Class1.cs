79fe7df60fcb6f6c465e071a26564d36e0abf861
길이스타트업 <51491577+gilitechmaster@users.noreply.github.com>
2021-08-11 01:39:40 +0900

    Create Class1.cs

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lang_UWP_2
+{
+    class Note
+    {
+        public string Filename { get; set; }
+        public string Filename2 { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
