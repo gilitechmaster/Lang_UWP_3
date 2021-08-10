7783b920afbe29f6f76f03d2fe76ab649fb9a0bb
길이스타트업 <51491577+gilitechmaster@users.noreply.github.com>
2021-08-11 02:55:30 +0900

    Update Page1.xaml.cs

--- Page1.xaml.cs
+++ Page1.xaml.cs
@@ -283,6 +283,104 @@ namespace Lang_UWP_2
             await Navigation.PopAsync();
         }
 
+        async void OnCalc5ButtonClicked(object sender, EventArgs e) // 종합분해버튼
+        {
+            var note = (Note)BindingContext;
+
+            if (string.IsNullOrWhiteSpace(note.Filename))
+            {
+                var filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
+                File.WriteAllText(filename, note.Text);
+            }
+
+            else
+            {
+                var filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
+                File.WriteAllText(note.Filename, note.Text);
+                StreamWriter sw = new StreamWriter(filename, true);
+                string[] arr = note.Text.Split(new string[]{
+
+                    ". ",  // 순차처리하므로 ". "를 앞에해두어야 한다.
+                    ".",
+                    ", ",
+                    ",",
+                    "? ",
+                    "?",
+                    "! ",
+                    "!",
+                    "·", // 법률문서에 사용되는 기호
+                    " "
+
+                        }, StringSplitOptions.None);
+
+                foreach (string B in arr)
+                {
+
+                    //sw.WriteLine(B);
+
+                    // 언어물리는 3변수로 정리한다.
+                    // 현재 3변수에서 시간은 고려하지 않는다.
+                    // 시간은 에너지와 동일한 클래스이다.
+
+                    // 1. 개체 2. 에너지 3. 기호(조사, 접속사, 기호 등)
+                    // 나는 집에 가다 = 나+집+가다 = 개체+개체+E
+
+                    // 개체 + 에너지 = 1최소요소
+                    // 1최소요소는 절대요소이다.
+                    // 최대절대요소는 1개의 띄어쓰기로 한정한다.
+                    // 띄어쓰기 갯수는 최대 2개까지로 임시값 한정한다.
+
+
+                    if (B.EndsWith("학") == true) // 개체
+                        sw.WriteLine("{0}", B
+                            .Replace("광화학", "광화학 = 개체")
+                            );
+
+                    if (B.EndsWith("성") == true) // 개체
+                        sw.WriteLine("{0}", B
+                            .Replace("반응성", "반응성 = 개체")
+                            );
+
+                    if (B.EndsWith("질") == true) // 개체
+                        sw.WriteLine("{0}", B
+                            .Replace("매질", "매질 = 개체")
+                            );
+
+                    if (B.EndsWith("다") == true) // 에너지
+                        sw.WriteLine("{0}", B
+
+                            .Replace("있다", "있")
+                            .Replace("있", "E = 100")
+
+                            .Replace("없다", "없")
+                            .Replace("없", "E = 0")
+                            );
+
+                    if (B.EndsWith("는") == true) // 기호
+                        sw.WriteLine("{0}", B
+                        .Replace("또는", "+")
+                        .Replace("는", "+")
+                        );
+
+                    if (B.EndsWith("이") == true) // 기호
+                        sw.WriteLine("{0}", B
+                        .Replace("이", "+")
+                        );
+
+                    if (B.EndsWith("서") == true) // 기호
+                        sw.WriteLine("{0}", B
+                        .Replace("에서", "서")
+                        .Replace("서", "+")
+                        );
+                }
+                sw.Close();
+            }
+
+            await Navigation.PopAsync();
+        }
+
+
+
         async void OnDeleteButtonClicked(object sender, EventArgs e) // 삭제버튼
         {
             var note = (Note)BindingContext;
